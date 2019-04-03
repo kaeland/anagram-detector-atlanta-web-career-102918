15c818275cc28d69c1694332c485809b385adcdb
Kaeland Chatman <kaeland1@gmail.com>
2019-04-03 06:12:59 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_array)
+    anagrams = words_array.find_all do |word|
+      word.split("").sort == self.word.split("").sort 
+    end
+  end
+end
